46ff39dad7b1c65101d3a1045f98f577373b9610
943432216 <35713053+943432216@users.noreply.github.com>
2018-02-28 17:49:17 +0800

    超
    数据更新

--- js/pc_main.js
+++ js/pc_main.js
@@ -49,7 +49,39 @@ $(function() {
 				$('.shade_nav').children('span').removeClass('sec_A');
 				$(this).children('span').addClass('sec_A');
 				$('.msg_box').eq(a).css('display','block');
+				if($(this).children('span').html()=='视频中心'){
+					$('.video_left,.video_right').css('display','block');
+				}else{
+					$('.video_left,.video_right').css('display','none');
+				}
 			}
 		})
+	});
+	//video
+	var n=0;
+	$('.video_left').click(function(){
+		n--;
+		if(n<=0){
+			alert('已经是第一个视频')
+		}else{
+			n--;
+		}
+		videold(n);
+		return n;
+	})
+	$('.video_right').click(function(){
+		if(n>4){
+			alert('已经是最后一个视频')
+		}else{
+			n++
+		}
+		videold(n)
+		return n;
 	})
 })
+
+
+function videold(num){
+	var arr=['http://player.youku.com/embed/XMTc2MzQ4NTI1Ng==','http://player.youku.com/embed/XMjQ3NzE0NTczMg==','http://player.youku.com/embed/XMTYwNzQ3ODA0OA==','http://player.youku.com/embed/XMTYwNzQ2ODY5Ng==','http://player.youku.com/embed/XMTUzNDI0NjUwNA=='];
+	$('#vidse').attr('src',arr[num]);
+}
