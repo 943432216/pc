7214eb29a1a1a83b9c840097041b1a6e7b43218b
943432216 <35713053+943432216@users.noreply.github.com>
2018-03-01 15:48:21 +0800

    超
    数据更新

--- js/pc_main.js
+++ js/pc_main.js
@@ -1,4 +1,5 @@
 $(function() {
+	//导航
 	$('.start').each(function() {
 		$(this).on({
 			mouseover: function() {
@@ -21,67 +22,98 @@ $(function() {
 			}
 		})
 	});
+	//二维码
 	$('.ewm').on({
-		click:function(){
-			$('.ewmx').css('display','block');
+		click: function() {
+			$('.ewmx').css('display', 'block');
 		},
-		mouseover:function(){
-			$('.ewmx').css('display','block');
+		mouseover: function() {
+			$('.ewmx').css('display', 'block');
 		},
-		mouseout:function(){
-			$('.ewmx').css('display','none');
+		mouseout: function() {
+			$('.ewmx').css('display', 'none');
 		}
 	});
-	$('.sec_title span').each(function(a,b){
+	$('.sec_title span').each(function(a, b) {
 		$(this).on({
-			mouseover:function(){
-				$('.sec_box').css('display','none');
+			mouseover: function() {
+				$('.sec_box').css('display', 'none');
 				$('.sec_title span').removeClass('sec_A');
 				$(this).addClass('sec_A')
-				$('.sec_box').eq(a).css('display','block');
+				$('.sec_box').eq(a).css('display', 'block');
 			}
 		})
 	});
-	$('.shade_nav').each(function(a,b){
+	//新闻
+	$('.shade_nav').each(function(a, b) {
 		$(this).on({
-			mouseover:function(){
-				$('.msg_box').css('display','none');
+			mouseover: function() {
+				$('.msg_box').css('display', 'none');
 				$('.shade_nav').children('span').removeClass('sec_A');
 				$(this).children('span').addClass('sec_A');
-				$('.msg_box').eq(a).css('display','block');
-				if($(this).children('span').html()=='视频中心'){
-					$('.video_left,.video_right').css('display','block');
-				}else{
-					$('.video_left,.video_right').css('display','none');
+				$('.msg_box').eq(a).css('display', 'block');
+				if($(this).children('span').html() == '视频中心') {
+					$('.video_left,.video_right').css('display', 'block');
+				} else {
+					$('.video_left,.video_right').css('display', 'none');
 				}
 			}
 		})
 	});
 	//video
-	var n=0;
-	$('.video_left').click(function(){
-		n--;
-		if(n<=0){
+	var n = 0;
+	$('.video_left').click(function() {
+		if(n <= 0) {
 			alert('已经是第一个视频')
-		}else{
+		} else {
 			n--;
 		}
 		videold(n);
 		return n;
 	})
-	$('.video_right').click(function(){
-		if(n>4){
+	$('.video_right').click(function() {
+		if(n >= 4) {
 			alert('已经是最后一个视频')
-		}else{
+		} else {
 			n++
 		}
 		videold(n)
 		return n;
 	})
+	//收藏or设为主页	
+	var urls = window.location.href;
+	var titles = document.title;
+	$('#collect').click(function() {
+		
+		AddFavorite(titles, urls);
+	})
+	$('#homepage').click(function(){
+		SetHome(urls)
+	})
 })
 
+function videold(num) {
+	var arr = ['http://player.youku.com/embed/XMTc2MzQ4NTI1Ng==', 'http://player.youku.com/embed/XMjQ3NzE0NTczMg==', 'http://player.youku.com/embed/XMTYwNzQ3ODA0OA==', 'http://player.youku.com/embed/XMTYwNzQ2ODY5Ng==', 'http://player.youku.com/embed/XMTUzNDI0NjUwNA=='];
+	$('#vidse').attr('src', arr[num]);
+}
 
-function videold(num){
-	var arr=['http://player.youku.com/embed/XMTc2MzQ4NTI1Ng==','http://player.youku.com/embed/XMjQ3NzE0NTczMg==','http://player.youku.com/embed/XMTYwNzQ3ODA0OA==','http://player.youku.com/embed/XMTYwNzQ2ODY5Ng==','http://player.youku.com/embed/XMTUzNDI0NjUwNA=='];
-	$('#vidse').attr('src',arr[num]);
+function AddFavorite(title, url) {
+	try {
+		window.external.addFavorite(url, title);
+	} catch(e) {
+		try {
+			window.sidebar.addPanel(title, url, "");
+		} catch(e) {
+			alert("抱歉，您所使用的浏览器无法完成此操作。\n\n请使用快捷键Ctrl+D进行添加！");
+		}
+	}
+}
+
+function SetHome(url) {
+	if(document.all) {
+		document.body.style.behavior = 'url(#default#homepage)';
+		document.body.setHomePage(url);
+	} else {
+		alert("您好,您的浏览器不支持自动设置页面为首页功能,请您手动在浏览器里设置该页面为首页!");
+	}
 }
