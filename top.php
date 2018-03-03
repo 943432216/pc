2c771ec8f1bfefbab3f7fcca11059dd9d7336fc1
943432216 <35713053+943432216@users.noreply.github.com>
2018-03-03 19:35:20 +0800

    zhang
    底部文件的替换

--- top.php
+++ top.php
@@ -1,133 +1,110 @@
-<?php
-include('inc/site_config.php');
-include($site_root_path.'/inc/set/ext_var.php');
-include($site_root_path.'/inc/fun/mysql.php');
-include($site_root_path.'/inc/function.php');
-include($site_root_path.'/inc/common.php');
-$pageName='index';
-?>
-<!DOCTYPE html>
-<html>
-
-	<head>
-		<meta charset="utf-8" />
-		<title>心宝药业</title>
-		<link rel="stylesheet" type="text/css" href="css/nitialize.css" />
-		<link rel="stylesheet" type="text/css" href="css/pc_main.css" />
-	</head>
-
-	<body>
-		<header class="header left width">
-			<div class="width left headr_title position">
-				<span class="ewmx"><img src="img/pc_ewm.png"/></span>
-				<span class="wel left">您好，心宝药业欢迎您！</span>
-				<span class="tel_right left">
-						<p>联系方式：（TEL）020-37924226 / 020-87817116 </p>
-						<p class="ewm"><img src="img/xewm.png"/></p>
-						<ul>
-							<li><a href="javascript:;">收藏本公司</a></li>
-							<li><a href="javascript:;">设为主页</a></li>
+<header class="header left width">
+	<div class="width left headr_title position">
+		<span class="ewmx"><img src="img/pc_ewm.png"/></span>
+		<span class="wel left">您好，心宝药业欢迎您！</span>
+		<span class="tel_right left">
+				<p>联系方式：（TEL）020-37924226 / 020-87817116 </p>
+				<p class="ewm"><img src="img/xewm.png"/></p>
+				<ul>
+					<li><a href="javascript:;">收藏本公司</a></li>
+					<li><a href="javascript:;">设为主页</a></li>
+				</ul>
+			</span>
+	</div>
+	<div class="left width header_nav position">
+		<div class="left nav_left">
+			<img src="img/pc_logo.png" />
+		</div>
+		<div class="left nav_right">
+			<ul>
+				<li>
+					<div>
+						<a href="<?=get_url('article',$art_group[1][0]);?>">首页</a>
+					</div>
+				</li>
+				<li>
+					<div class="start">
+						<a href="<?=get_url('article',$art_group[1][0]);?>">关于心宝</a>
+					</div>
+					<div class="nav2">
+						<?php
+						if(!empty($art_group[1])){
+						?>
+						<ul>	
+							<?php foreach((array)$art_group[1] as $item){?>						
+							<li>
+								<a href="<?=get_url('article',$item)?>"><?=$item['Title']?></a>
+							</li>
+							<?php }?>
 						</ul>
-					</span>
-			</div>
-			<div class="left width header_nav position">
-				<div class="left nav_left">
-					<img src="img/pc_logo.png" />
-				</div>
-				<div class="left nav_right">
-					<ul>
-						<li>
-							<div>
-								<a href="<?=get_url('article',$art_group[1][0]);?>">首页</a>
-							</div>
-						</li>
-						<li>
-							<div class="start">
-								<a href="<?=get_url('article',$art_group[1][0]);?>">关于心宝</a>
-							</div>
-							<div class="nav2">
-								<?php
-								if(!empty($art_group[1])){
-								?>
-								<ul>	
-									<?php foreach((array)$art_group[1] as $item){?>						
-									<li>
-										<a href="<?=get_url('article',$item)?>"><?=$item['Title']?></a>
-									</li>
-									<?php }?>
-								</ul>
-								<?php }?>
-							</div>
-						</li>
-						<li>
-							<div class="start">
-								<a href="/products.php">产品中心</a>
-							</div>
-							<div class="nav2">
-								<?php
-								if(!empty($product_cate)){
-								?>
-								<ul>
-									<?php foreach((array)$product_cate as $item){?>
-									<li>
-										<a href="<?=get_url('product_category',$item)?>"><?=$item['Category']?></a>
-									</li>
-									<?php }?>
-								</ul>
-								<?php }?>
-							</div>
-						</li>
-						<li>
-							<div class="start">
-								<a href="<?=get_url('info2_category',$info2_cate[0]);?>">心肾同治</a>
-							</div>
-							<div class="nav2">
-								<?php
-								if(!empty($info2_cate)){
-								?>
-								<ul>
-									<?php foreach((array)$info2_cate as $item){?>
-									<li>
-										<a href="<?=get_url('info2_category',$item)?>"><?=$item['Category']?></a>
-									</li>
-									<?php }?>
-								</ul>
-								<?php }?>
-							</div>
-						</li>
-						<li>
-							<div class="start">
-								<a href="/info.php?CateId=1">最新动态</a>
-							</div>
-							<div class="nav2">
-								<?php
-								if(!empty($info_cate)){
-								?>
-								<ul>
-									<?php
-										foreach((array)$info_cate as $item){
-											if($item['CateId'] == 7){continue;}
-									?>
-									<li>
-										<a href="<?=get_url('info_category',$item)?>"><?=$item['Category']?></a>
-									</li>
-									<?php }?>
-								</ul>
-								<?php }?>
-							</div>
-						</li>
-						<li>
-							<div>
-								<a href="#">联系心宝</a>
-							</div>
-						</li>
-					</ul>
-
-				</div>
-			</div>
-		</header>
-		<script src="js/jquery-2.1.1.min.js" type="text/javascript" charset="utf-8"></script>
-		<script src="js/pc_main.js" type="text/javascript" charset="utf-8"></script>
-	</body>
-
-</html>
+						<?php }?>
+					</div>
+				</li>
+				<li>
+					<div class="start">
+						<a href="/products.php">产品中心</a>
+					</div>
+					<div class="nav2">
+						<?php
+						if(!empty($product_cate)){
+						?>
+						<ul>
+							<?php foreach((array)$product_cate as $item){?>
+							<li>
+								<a href="<?=get_url('product_category',$item)?>"><?=$item['Category']?></a>
+							</li>
+							<?php }?>
+						</ul>
+						<?php }?>
+					</div>
+				</li>
+				<li>
+					<div class="start">
+						<a href="<?=get_url('info2_category',$info2_cate[0]);?>">心肾同治</a>
+					</div>
+					<div class="nav2">
+						<?php
+						if(!empty($info2_cate)){
+						?>
+						<ul>
+							<?php foreach((array)$info2_cate as $item){?>
+							<li>
+								<a href="<?=get_url('info2_category',$item)?>"><?=$item['Category']?></a>
+							</li>
+							<?php }?>
+						</ul>
+						<?php }?>
+					</div>
+				</li>
+				<li>
+					<div class="start">
+						<a href="/info.php?CateId=1">最新动态</a>
+					</div>
+					<div class="nav2">
+						<?php
+						if(!empty($info_cate)){
+						?>
+						<ul>
+							<?php
+								foreach((array)$info_cate as $item){
+									if($item['CateId'] == 7){continue;}
+							?>
+							<li>
+								<a href="<?=get_url('info_category',$item)?>"><?=$item['Category']?></a>
+							</li>
+							<?php }?>
+						</ul>
+						<?php }?>
+					</div>
+				</li>
+				<li>
+					<div>
+						<a href="#">联系心宝</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</header>
+<script src="js/jquery-2.1.1.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/pc_main.js" type="text/javascript" charset="utf-8"></script>
