17712a21b7ad549f9e5f88f9b9cf103f66e535a0
943432216 <35713053+943432216@users.noreply.github.com>
2018-03-03 17:32:27 +0800

    zhang
    补充后台文件，以及网站页面的头部文件修改

---
+++ index.php
@@ -0,0 +1,495 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8" />
+		<title>心宝药业</title>
+		<link rel="stylesheet" type="text/css" href="css/nitialize.css" />
+		<link rel="stylesheet" type="text/css" href="css/pc_main.css" />
+	</head>
+
+	<body>
+		<div class="xb_box overflow">
+			<header class="header left width">
+				<div class="width left headr_title position">
+					<span class="ewmx"><img src="img/pc_ewm.png"/></span>
+					<span class="wel left">你好，心宝药业欢迎您！</span>
+					<span class="tel_right left">
+						<p>联系方式：（TEL）020-37924226 / 020-87817116 </p>
+						<p class="ewm"><img src="img/xewm.png"/></p>
+						<ul>
+							<li><a href="javascript:;" id="collect">收藏本公司</a></li>
+							<li><a href="javascript:;" id="homepage">设为主页</a></li>
+						</ul>
+					</span>
+				</div>
+				<div class="left width header_nav position">
+					<div class="left nav_left">
+						<img src="img/pc_logo.png" />
+					</div>
+					<div class="left nav_right">
+						<ul>
+							<li>
+								<div class="navstax">
+									<a href="#">首页</a>
+								</div>
+							</li>
+							<li>
+								<div class="start">
+									<a href="#">关于心宝</a>
+								</div>
+								<div class="nav2">
+									<ul>
+										<li>
+											<a href="">公司简介</a>
+										</li>
+										<li>
+											<a href="">董事长致词</a>
+										</li>
+										<li>
+											<a href="">发展历程</a>
+										</li>
+										<li>
+											<a href="">企业文化</a>
+										</li>
+										<li>
+											<a href="">企业荣誉</a>
+										</li>
+									</ul>
+								</div>
+							</li>
+							<li>
+								<div class="start">
+									<a href="#">产品中心</a>
+								</div>
+								<div class="nav2">
+									<ul>
+										<li>
+											<a href="">心宝丸</a>
+										</li>
+										<li>
+											<a href="">龟鹿补肾片</a>
+										</li>
+										<li>
+											<a href="">蒲地蓝消炎片</a>
+										</li>
+										<li>
+											<a href="">蒲地蓝消炎片</a>
+										</li>
+									</ul>
+								</div>
+							</li>
+							<li>
+								<div class="start">
+									<a href="#">心肾同治</a>
+								</div>
+								<div class="nav2">
+									<ul>
+										<li>
+											<a href="">心肾相交理论</a>
+										</li>
+										<li>
+											<a href="">心宝丸的临床应用</a>
+										</li>
+										<li>
+											<a href="">龟鹿补肾片健康手册</a>
+										</li>
+									</ul>
+								</div>
+							</li>
+							<li>
+								<div class="start">
+									<a href="#">最新动态</a>
+								</div>
+								<div class="nav2">
+									<ul>
+										<li>
+											<a href="">公司动态</a>
+										</li>
+										<li>
+											<a href="">公司动态</a>
+										</li>
+										<li>
+											<a href="">视频中心</a>
+										</li>
+									</ul>
+								</div>
+							</li>
+							<li>
+								<div>
+									<a href="#">联系心宝</a>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</header>
+			<div class="banner float width">
+				<div id="marquee">
+					<ul>
+						<li>
+							<a href="javascript:;"><img src="img/banner.jpg" class="img"></a>
+						</li>
+						<li>
+							<a href="javascript:;"><img src="img/banner_01.jpg" class="img"></a>
+						</li>
+						<li>
+							<a href="javascript:;"><img src="img/banner_02.jpg" class="img"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<section class="left position">
+				<span class="video_left"><img src="img/video_left.png"/></span>
+				<span class="video_right"><img src="img/video_right.png"/></span>
+				<div class="sec_stl"><img src="img/pc_msg.png" /></div>
+				<div class="sec_titles left width">
+					<div class="shade overflow shade_nav">
+						<span class="sec_A">公司动态</span>
+					</div>
+					<div class="shade overflow shade_nav">
+						<span>行业动态</span>
+					</div>
+					<div class="shade overflow shade_nav">
+						<span>视频中心</span>
+					</div>
+				</div>
+				<div class="sec_con">
+					<div class="msg_box" style="display: block;">
+						<div class="msg_1">
+							<div class="msg_img">
+								<img src="img/686a9d6efb.png" class="img" />
+							</div>
+							<div class="msg_con">
+								<h2>我们等您来｜欢迎莅临第78届药交会心宝药业展位参观洽谈</h2>
+								<p>医药行业最具权威影响力与规模的第78届全国药品交易会在广州国际会展中心举行。广东心宝药业科技有限公司作为本届药交会重要参展商，在B区12.2展馆12.2E32特装展位盛装开幕。</p>
+								<a href="#">查看详情 》》</a>
+							</div>
+						</div>
+						<div class="msg_1">
+							<div class="msg_img">
+								<img src="img/686a9d6efb.png" class="img" />
+							</div>
+							<div class="msg_con">
+								<h2>我们等您来｜欢迎莅临第78届药交会心宝药业展位参观洽谈</h2>
+								<p>医药行业最具权威影响力与规模的第78届全国药品交易会在广州国际会展中心举行。广东心宝药业科技有限公司作为本届药交会重要参展商，在B区12.2展馆12.2E32特装展位盛装开幕。</p>
+								<a href="#">查看详情 》》</a>
+							</div>
+						</div>
+						<div class="msg_1">
+							<div class="msg_img">
+								<img src="img/686a9d6efb.png" class="img" />
+							</div>
+							<div class="msg_con">
+								<h2>我们等您来｜欢迎莅临第78届药交会心宝药业展位参观洽谈</h2>
+								<p>医药行业最具权威影响力与规模的第78届全国药品交易会在广州国际会展中心举行。广东心宝药业科技有限公司作为本届药交会重要参展商，在B区12.2展馆12.2E32特装展位盛装开幕。</p>
+								<a href="#">查看详情 》》</a>
+							</div>
+						</div>
+					</div>
+					<div class="msg_box">
+						<div class="msg_1">
+							<div class="msg_img">
+								<img src="img/686a9d6efb.png" class="img" />
+							</div>
+							<div class="msg_con">
+								<h2>我们等您来｜欢迎莅临第78届药交会心宝药业展位参观洽谈</h2>
+								<p>医药行业最具权威影响力与规模的第78届全国药品交易会在广州国际会展中心举行。广东心宝药业科技有限公司作为本届药交会重要参展商，在B区12.2展馆12.2E32特装展位盛装开幕。</p>
+								<a href="#">查看详情 》》</a>
+							</div>
+						</div>
+						<div class="msg_1">
+							<div class="msg_img">
+								<img src="img/686a9d6efb.png" class="img" />
+							</div>
+							<div class="msg_con">
+								<h2>我们等您来｜欢迎莅临第78届药交会心宝药业展位参观洽谈</h2>
+								<p>医药行业最具权威影响力与规模的第78届全国药品交易会在广州国际会展中心举行。广东心宝药业科技有限公司作为本届药交会重要参展商，在B区12.2展馆12.2E32特装展位盛装开幕。</p>
+								<a href="#">查看详情 》》</a>
+							</div>
+						</div>
+						<div class="msg_1">
+							<div class="msg_img">
+								<img src="img/686a9d6efb.png" class="img" />
+							</div>
+							<div class="msg_con">
+								<h2>我们等您来｜欢迎莅临第78届药交会心宝药业展位参观洽谈</h2>
+								<p>医药行业最具权威影响力与规模的第78届全国药品交易会在广州国际会展中心举行。广东心宝药业科技有限公司作为本届药交会重要参展商，在B区12.2展馆12.2E32特装展位盛装开幕。</p>
+								<a href="#">查看详情 》》</a>
+							</div>
+						</div>
+					</div>
+					<div class="msg_box position">
+
+						<div class="big_video">
+							<iframe id="vidse" width="100%" height="100%" frameborder="no" scrolling="no" src='http://player.youku.com/embed/XMTc2MzQ4NTI1Ng=='></iframe>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="left position sec_bg">
+				<div class="sec_proi"><img src="img/PC_product_tl.png" /></div>
+				<div class="index_dw"></div>
+				<a href="javascript:;" id="prev" class="unslider-arrow prev"></a>
+				<a href="javascript:;" id="next" class="unslider-arrow next"></a>
+				<div id="product_mar" class="sec_con section_pro">
+					<ul>
+						<li>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+						</li>
+						<li>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+						</li>
+						<li>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+							<div class="pro_box">
+								<div class="six_box position">
+									<p class="pro_name">龟鹿补肾片</p>
+									<div class="bx_1"></div>
+									<div class="bx_2"><img src="img/59ac9eae09.jpg" /></div>
+									<div class="bx_3"></div>
+								</div>
+								<!--<div class="pro_details">
+									<span class="left pro_de">查看更多</span>
+								</div>-->
+							</div>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<div class="left blogoll">
+				<div class="left blogoll_left">友情链接：</div>
+				<div class="left blogoll_right">
+					<a href="http://www.360kad.com/product/575808.shtml">龟鹿补肾片-康爱多网上药店</a>
+					<a href="http://www.gdda.gov.cn/publicfiles/business/htmlfiles/jsjzz/index.htm">广东省食品药品监督管理总局</a>
+					<a href="http://www.sda.gov.cn/WS01/CL0001/">国家食品药品监督管理总局</a>
+				</div>
+			</div>
+			<footer class="left width">
+				<div class="left width footer_top position">
+					<div class="logob"><img src="img/pc_logob.png" /></div>
+					<span class="footer_nav left">
+					<h2>关于心宝</h2>
+					<ul>
+						<li>
+							<a href="">&nbsp;-&nbsp;公司简介</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;发展历程</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;企业文化</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;企业荣誉</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;董事长致词</a>
+						</li>
+					</ul>
+				</span>
+					<span class="footer_nav left">
+					<h2>产品中心</h2>
+					<ul>
+						<li>
+							<a href="">&nbsp;-&nbsp;心宝丸</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;龟鹿补肾片</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;蒲地蓝消炎片</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;蒲地蓝消炎胶囊</a>
+						</li>
+						<!--<li>
+							<a href="">&nbsp;-&nbsp;董事长致词</a>
+						</li>-->
+					</ul>
+				</span>
+					<span class="footer_nav left">
+					<h2>心肾同治</h2>
+					<ul>
+						<li>
+							<a href="">&nbsp;-&nbsp;心肾相交理论</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;心宝丸的临床应用</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;龟鹿补肾片健康手册</a>
+						</li>
+						<!--<li>
+							<a href="">&nbsp;-&nbsp;企业荣誉</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;董事长致词</a>
+						</li>-->
+					</ul>
+				</span>
+					<span class="footer_nav left">
+					<h2>最新动态</h2>
+					<ul>
+						<li>
+							<a href="">&nbsp;-&nbsp;公司动态</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;行业动态</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;视频中心</a>
+						</li>
+						<!--<li>
+							<a href="">&nbsp;-&nbsp;企业荣誉</a>
+						</li>
+						<li>
+							<a href="">&nbsp;-&nbsp;董事长致词</a>
+						</li>-->
+					</ul>
+				</span>
+					<span class="footer_navs">
+					<h2>联系我们</h2>
+					<ul>
+						<li>
+							企业名称：广东心宝药业科技有限公司
+						</li>
+						<li>
+							生产地址：广州高新技术产业开发区伴河路6号
+						</li>
+						<li>
+							电话号码：020-37924226 020-87817116 
+						</li>
+						<li>
+							传真号码：020-87817116
+						</li>
+						<li>
+							邮编号码：510535
+						</li>
+					</ul>
+				</span>
+				</div>
+				<div class="left width footer_bn">
+					《互联网药品信息服务资格证》编号（粤）-非经营性-2011-0122&nbsp;|&nbsp;Copyright © 广东心宝制药有限公司&nbsp;&nbsp;&nbsp;&nbsp;粤ICP备案：10025609号
+				</div>
+			</footer>
+		</div>
+
+		<script src="js/jquery-2.1.1.min.js" type="text/javascript" charset="utf-8"></script>
+		<script src="js/unslider.min.js" type="text/javascript" charset="utf-8"></script>
+		<script src="js/pc_main.js" type="text/javascript" charset="utf-8"></script>
+		<script type="text/javascript">
+			$(function() {
+			//banner轮播
+				$('#marquee ul li').height($('.img').height());
+				var unslider06 = $('#marquee').unslider({
+					dots: true,
+					fluid: true,
+					speed:500,
+					delay:5000
+				});
+				data06 = unslider06.data('unslider');
+			//产品轮播
+   				$('#product_mar ul li').css('height','550');
+    			var unslider = $('#product_mar').unslider({
+    				dots: true,
+					fluid: true,
+					speed:200,
+					delay:10000
+    			});
+
+   				 $('.unslider-arrow').click(function() {
+        			var fn = this.className.split(' ')[1];
+      			  	unslider.data('unslider')[fn]();
+				});
+
+			})
+		</script>
+	</body>
+
+</html>
